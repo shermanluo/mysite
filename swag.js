3d62e57640d791d9b02f690d449dd362edcc69f8
DESKTOP-AI6RF7O\Sherman Luo <shermanluo@berkeley.edu>
2017-06-14 16:36:15 -0700

    d

--- swag.js
+++ swag.js
@@ -16,7 +16,10 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://heroku_7xmghlz3:se7igu36ap1lboi5h4rgfk5eha@ds127132.mlab.com:27132/heroku_7xmghlz3";
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(htmlPath));
-
+app.get('/', function (req, res) {
+ 
+  res.sendFile(htmlPath + '/MyWebsite.html');
+})
 app.post('/', function(req, res) {
   console.log("done");
   var mailOptions = {
@@ -50,7 +53,6 @@ res.sendStatus(204);
 
 
 var server = app.listen(process.env.PORT || 5000, function () {
-  
 });
 
 
