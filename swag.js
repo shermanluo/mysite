b398d3e5d2514c3d87e8c04f4b4693c160dfecb4
DESKTOP-AI6RF7O\Sherman Luo <shermanluo@berkeley.edu>
2017-06-14 00:41:36 -0700

    don

--- swag.js
+++ swag.js
@@ -26,7 +26,7 @@ app.post('/', function(req, res) {
   subject: req.body.firstname + " " + req.body.lastname,
   text: req.body.message
   };
-  console.log("req.body.firstname");
+  console.log(req.body.firstname);
   transporter.sendMail(mailOptions, function(error, info){
   console.log("HELLO");
   if (error) {
