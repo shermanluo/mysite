1522ffa672ea4b7f549df0f53e0de0ce86d511e4
DESKTOP-AI6RF7O\Sherman Luo <shermanluo@berkeley.edu>
2017-06-17 03:52:23 -0700

    d

--- swag.js
+++ swag.js
@@ -55,16 +55,7 @@ app.get('*', function (req, res) {
   res.sendFile(htmlPath + '/MyWebsite.html');
 })
 
-app.all('*', function(req, res, next) {	
-console.log("second");
-	console.log('https://' + req.hostname + req.originalUrl);   
-	console.log(req.protocol === 'https');
-	if (!req.protocol === 'https') {return res.redirect('https://' + req.hostname + req.originalUrl);
-	} else {
-		console.log("going");
-		next();
-	}
-}); 
+ 
 
 var server = app.listen(process.env.PORT || 5000, function () {
 });
